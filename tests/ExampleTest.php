6351c78d42676bbe2e6a240d6e169bd29d3eaac4
Ginemiagl <Martirosyan.M@postuf.com>
2020-08-17 17:26:51 +0300

    Add travise.ci - add configuration for codcov

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Test;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_example(): void
+    {
+        self::assertEquals(1, 1, 'for test');
+    }
+
+}
